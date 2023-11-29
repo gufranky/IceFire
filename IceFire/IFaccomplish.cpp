6d037a436520cf0a537b01c4708c0d4292368d9a
gufranky <g2676536169@gmail.com>
2023-11-29 22:50:06 +0800

    加了检查，为后续做准备

--- IceFire/IFaccomplish.cpp
+++ IceFire/IFaccomplish.cpp
@@ -3,6 +3,10 @@ void IFaccomplish::SetPos(int xx, int yy)
 {
 	x = xx;
 	y = yy;
+	if (x < 0) x = 0;
+	if (x > 1900) x = 1900;
+	if (y < 0) y = 0;
+	if (y > 1050) y = 1050;
 	setPos(x, y);
 }
 void IFaccomplish::PosChange(int Deltax, int Deltay)
