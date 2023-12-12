2fddef7c77a156363f3ae9d0c76c6d14a5bbae13
家琛 范 <1136101561@qq.com>
2023-12-12 11:38:02 +0800

    关卡选择限制

--- IceFire/LevelChoose.cpp
+++ IceFire/LevelChoose.cpp
@@ -1,6 +1,7 @@
 #include "LevelChoose.h"
 LevelChoose::LevelChoose(QWidget* parent) :
 	QWidget(parent)
+
 {
 	LevelButton = new QPushButton[20];
 	for (int i = 0; i < 4; i++)
@@ -21,15 +22,50 @@ LevelChoose::LevelChoose(QWidget* parent) :
 				"QPushButton:hover {"
 				"    background-color: lightblue;"  // 鼠标悬停时的背景色
 				"}");
-			
+
 			QObject::connect(&LevelButton[i * 5 + j], &QPushButton::clicked, this, [=]()
 				{
-					emit level(i * 5 + j + 1);
+					handleLevelButtonClick(i * 5 + j + 1);
 				}
 			);
 		}
 }
+int LevelChoose::currentLevel = 1;
+int LevelChoose::levelCompleted = 0;
+
 LevelChoose::~LevelChoose()
 {
-	delete[] LevelButton;
+	delete LevelButton;
+}
+
+void LevelChoose::handleLevelButtonClick(int Level)
+{
+	// 如果点击的关卡小于当前关卡，已完成
+	if (Level <= currentLevel)
+	{
+		emit level(Level);
+		levelCompleted = std::max(levelCompleted, Level);
+		// 解锁下一关
+		if (Level == currentLevel)
+			unlockNextLevel();
+	}
+	else 
+	{
+		// 提示未完成前置关卡
+		qDebug() << "请先完成前置关卡";
+	}
+}
+void LevelChoose::unlockNextLevel()
+{
+	// 解锁下一关
+	currentLevel++;
+
+	// 更新按钮状态
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+		{
+			int level = i * 5 + j + 1;
+			LevelButton[i * 5 + j].setEnabled(level <= currentLevel);
+//只有当前解锁的关卡及之前的关卡的按钮才是可点击的，而未解锁的关卡的按钮点击无效
+		}
 }
